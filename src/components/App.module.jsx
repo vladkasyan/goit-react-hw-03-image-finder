c3b0b7ddc1e7927d1de961ad1dfd7d28765bd69e
vladkasyan <voadkasyan.2006@gmail.com>
2023-09-10 11:49:55 +0300

    start

---
+++ src/components/App.module.jsx
@@ -0,0 +1,10 @@
+import styled from 'styled-component'
+
+export const Container = styled.div`
+    display: grid;
+    grid-template-columns: 1fr;
+    grid-gap: 16px;
+    padding-bottom: 24px;
+`
+
+
--- src/components/Searchbar/Searchbar.jsx
+++ src/components/Searchbar/Searchbar.jsx
@@ -1,1 +1,52 @@
-import PropTypes from "prop-types";
+import { Component } from 'react';
+import { toast } from 'react-toastify';
+
+import { SearchBar, SearchForm, SearchButton, SearchButtonLabel, SearchInput } from './Searchbar.module';
+
+export default class SearchBar extends Component {
+    state = {
+        searchQuery: ``
+    };
+
+    SubmitForm = event => {
+        const searchQuery = this.state.searchQuery.trim()
+        event.preventDefault()
+
+
+        if (searchQuery.trim() === "") {
+            toast.info('Please, enter search word!');
+            return
+        }
+
+        this.props.onSubmit(searchQuery);
+        this.setState({searchQuery: ''})
+    }
+
+    queryChange = ({currnetTarget : {value} }) => {
+        this.setState({ searchQuery: value.toLowerCase()})
+    }
+
+    render() {
+        const {searchQuery} = this.state
+        return (
+            <SearchBar class="searchbar">
+    <SearchForm class="form">
+    <SearchButton type="submit" class="button">
+        <SearchButtonLabel class="button-label">Search</SearchButtonLabel>
+    </SearchButton>
+
+    <SearchInput
+        class="input"
+        type="text"
+        autocomplete="off"
+        autofocus
+        placeholder="Search images and photos"
+    />
+    </SearchForm>
+</SearchBar>
+        )
+    }
+
+}
+    
+
